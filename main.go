4dd27a45f3fa132caf898c9093e110e67d66a716
Lakshya Singh <lakshay.singh1108@gmail.com>
2023-04-09 11:13:47 +0530

    feat: unique id generator
    timestamp src dest local uniq
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -17,6 +19,23 @@ func main() {
 		}
 
 		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	uniqueID := 1
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+
+		id := fmt.Sprintf("%d-%s-%s-%d", time.Now().Unix(), msg.Src, msg.Dest, uniqueID)
+		uniqueID += 1
+
+		body["id"] = id
 
 		return n.Reply(msg, body)
 	})
