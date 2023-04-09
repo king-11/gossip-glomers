2be82a1546757dd76f7f3130df4ac3b79ad9e096
Lakshya Singh <lakshay.singh1108@gmail.com>
2023-04-09 18:18:52 +0530

    feat: 3a read working broadcast
    keep seens in set and return them
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- main.go
+++ main.go
@@ -40,6 +40,48 @@ func main() {
 		return n.Reply(msg, body)
 	})
 
+	seen := make(map[int]struct{})
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		type Body struct {
+			TypeOf  string `json:"type"`
+			Message int    `json:"message"`
+		}
+
+		var body Body
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		seen[body.Message] = struct{}{}
+
+		resp_body := make(map[string]any)
+		resp_body["type"] = "broadcast_ok"
+
+		return n.Reply(msg, resp_body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		body := make(map[string]any)
+		vals := make([]int, 0)
+
+		for val := range seen {
+			vals = append(vals, val)
+		}
+
+		body["type"] = "read_ok"
+		body["messages"] = vals
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		body := make(map[string]any)
+
+		body["type"] = "topology_ok"
+
+		return n.Reply(msg, body)
+	})
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
